fa3eb0e7c08ffc637d89a82bd8cf9cd4652f2200
rubenquadros <rquadros95@gmail.com>
2021-11-26 22:03:23 +0530

    checking auth header

--- index.js
+++ index.js
@@ -12,27 +12,44 @@ app.use(helmet())
 app.use(compression())
 
 const port = process.env.PORT || 3000
+const auth_header = 'x-auth-token'
 
 create_server()
 
 function create_server() {
 
     app.post('/login', async (req, res) => {
+        if (!isAuthorized(req.headers[auth_header])) {
+            res.status(401).send()
+            return
+        }
         const response = await login_service.login(req.body.id, req.body.name, req.body.email, req.body.profile_pic)
         res.status(response).send()
     })
 
-    app.get('/competitions', async (_, res) => {
+    app.get('/competitions', async (req, res) => {
+        if (!isAuthorized(req.headers[auth_header])) {
+            res.status(401).send()
+            return
+        }
         const response = await competitions_service.get_all_competitions()
         res.status(response.code).json(response.response_body)
     })
 
     app.post('/save_team', async (req, res) => {
+        if (!isAuthorized(req.headers[auth_header])) {
+            res.status(401).send()
+            return
+        }
         const response = await fav_team_service.save_team(req.body.id, req.body.user_id, req.body.name)
         res.status(response).send()
     })
 
-    app.get('/live_matches', async (_, res) => {
+    app.get('/live_matches', async (req, res) => {
+        if (!isAuthorized(req.headers[auth_header])) {
+            res.status(401).send()
+            return
+        }
         const response = await live_matches_service.get_live_matches()
         res.status(response.code).json(response.response_body)
     })
@@ -40,4 +57,8 @@ function create_server() {
     app.listen(port, () => {
         console.log('Staring server')
     })
+}
+
+function isAuthorized(token) {
+    return token == process.env.X_AUTH_TOKEN
 }
